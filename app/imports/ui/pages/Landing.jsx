3f00fc5a2d80efb46f3fc933a4a7a6cbeef1ebc4
Connor Sonoda <43054889+csonoda@users.noreply.github.com>
2024-03-28 10:37:09 -1000

    Update Landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => (
           Welcome to the Workout App
         </h1>
         <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-          Profiles, projects, and interest areas for the UH Community
+          View and share your workout routines with others!
         </h3>
       </Container>
     </div>
@@ -30,7 +30,7 @@ const Landing = () => (
     </div>
     <div className="landing-green-background">
       <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...then add your projects</h2>
+        <h2 style={{ color: 'white' }}>...then add your routines</h2>
         <Row md={1} lg={2}>
           <Col xs={6}>
             <Image src="/images/add-project-page.png" width={500} />
